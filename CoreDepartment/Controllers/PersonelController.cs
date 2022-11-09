ac322161cad87fcc01ecd3f9841cb5331e2a8748
akoyuncuoglu <akoyuncuoglu@PC-KOYUNCUOGLU>
2022-11-09 16:59:34 +0300

    create personel view updated

--- CoreDepartment/Controllers/PersonelController.cs
+++ CoreDepartment/Controllers/PersonelController.cs
@@ -1,5 +1,7 @@
 ﻿using CoreDepartment.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace CoreDepartment.Controllers
@@ -12,6 +14,21 @@ namespace CoreDepartment.Controllers
         {
             var values = c.Personels.ToList();
             return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult NewPersonel()
+        {
+            List<SelectListItem> values = (from x in c.Departments.ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text = x.DepartmentName,
+                                               Value = x.Id.ToString()
+                                           }
+                                         ).ToList();
+
+            ViewBag.val = values;
+            return View();
         }
     }
 }

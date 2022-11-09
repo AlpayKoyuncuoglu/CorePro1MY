5bfeb71cc815cb25ae40ca69aac35ee28f949294
akoyuncuoglu <akoyuncuoglu@PC-KOYUNCUOGLU>
2022-11-09 15:14:34 +0300

    personel added

---
+++ CoreDepartment/Controllers/PersonelController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDepartment.Controllers
+{
+    public class PersonelController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

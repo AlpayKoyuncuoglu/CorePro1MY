cead4335a382d95fa7435887fa231b2d8b26866d
akoyuncuoglu <akoyuncuoglu@PC-KOYUNCUOGLU>
2022-11-10 14:01:17 +0300

    temp_added

---
+++ CoreDepartment/Controllers/LoginController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDepartment.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Login()
+        {
+            return View ();
+        }
+    }
+}
